43cf71eadf610fd9c205a67690fbe6bb6307376f
mdnizam-nextloop <mdnizam@nextlooptechnologies.com>
2025-07-08 23:36:48 +0530

    added login ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,8 +26,14 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        /> */}
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
         {children}
       </body>
