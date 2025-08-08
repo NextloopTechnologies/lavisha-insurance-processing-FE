995f82ca8a19eec4aa55c5ea785725cfa094dd37
mdnizam-nextloop <mdnizam@nextlooptechnologies.com>
2025-08-08 11:20:53 +0530

    all feature tested ok

---
+++ app/claims/ClaimsContent.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { DataTable } from "@/components/DataTable";
+import DeletePopup from "@/components/DeletePopup";
+import LoadingOverlay from "@/components/LoadingOverlay";
+import SidebarLayout from "@/components/SidebarLayout";
+import { deleteClaims, getClaimsByParams } from "@/services/claims";
+import { format } from "date-fns";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function ClaimsContent() {
+  const [loading, setLoading] = useState(false);
+  const [claims, setClaims] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [total, setTotal] = useState(0);
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [selectedId, setSelectedId] = useState<string | null>(null);
+  const [searchData, setSearchData] = useState({
+    selectedStatuses: [],
+    selectedDate: "",
+    debouncedSearchTerm: "",
+  });
+
+  const getSearchData = (value, name) => {
+    setSearchData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const searchParams = useSearchParams();
+  const patientNameFromQuery = searchParams.get("patientName");
+
+  const fetchClaims = async () => {
+    setLoading(true);
+    try {
+      const query: any = {
+        skip: (page - 1) * pageSize,
+        take: pageSize,
+        sortBy: "createdAt",
+        sortOrder: "desc",
+      };
+
+      if (searchData.debouncedSearchTerm) {
+        query.patientName = searchData.debouncedSearchTerm;
+      }
+      if (searchData.selectedStatuses.length > 0) {
+        query.status = searchData.selectedStatuses.join(",");
+      }
+      if (searchData.selectedDate) {
+        query.createdFrom = format(
+          new Date(searchData.selectedDate),
+          "yyyy-MM-dd"
+        );
+      }
+
+      const res = await getClaimsByParams(query);
+      if (res?.status === 200) {
+        setClaims(res.data.data);
+        setTotal(Math.ceil(res.data.total / pageSize));
+      }
+    } catch (err) {
+      console.error("Failed to fetch claims:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (patientNameFromQuery) {
+      setSearchData((prev) => ({
+        ...prev,
+        debouncedSearchTerm: patientNameFromQuery,
+      }));
+    }
+  }, [patientNameFromQuery]);
+
+  useEffect(() => {
+    fetchClaims();
+  }, [page, pageSize, searchData]);
+
+  const handleDeleteClaim = (id: string) => {
+    setSelectedId(id);
+    setOpenDeleteDialog(true);
+  };
+
+  const confirmDelete = async () => {
+    setOpenDeleteDialog(false);
+    const res = await deleteClaims(selectedId);
+    if (res?.status === 200) {
+      fetchClaims();
+    }
+  };
+
+  return (
+    <SidebarLayout>
+      {loading && <LoadingOverlay />}
+      <div className="bg-gray-100">
+        <DataTable
+          data={claims}
+          page={page}
+          setPage={setPage}
+          total={total}
+          sortByClaim={() => {}}
+          handleDeleteClaim={handleDeleteClaim}
+          getSearchData={getSearchData}
+          initialSearchTerm={patientNameFromQuery}
+        />
+
+        <DeletePopup
+          open={openDeleteDialog}
+          onOpenChange={setOpenDeleteDialog}
+          onConfirm={confirmDelete}
+          title="Do you want to delete this Claim?"
+          confirmText="Delete"
+          cancelText="Cancel"
+        />
+      </div>
+    </SidebarLayout>
+  );
+}
---
+++ app/newClaim/NewClaimsContent.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import FileDrag from "@/components/FileDrag";
+import SidebarLayout from "@/components/SidebarLayout";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { INSURANCE_COMPANIES, TPA_OPTIONS } from "@/constants/menu";
+import { createClaims, getClaimsById } from "@/services/claims";
+import { bulkUploadFiles, uploadFiles } from "@/services/files";
+import { getPatientById, getPatients } from "@/services/patients";
+import { useRouter, useSearchParams } from "next/navigation";
+
+// import { Textarea } from "@/components/ui/textarea"
+import { Plus, Search, UploadCloud } from "lucide-react";
+import { useParams } from "next/navigation";
+import { useEffect, useState, } from "react";
+import Image from "next/image";
+import { userRound } from "@/assets";
+import PatientFormDialog from "@/components/CreateEdit";
+import LoadingOverlay from "@/components/LoadingOverlay";
+import CreateClaim from "@/components/CreateClaim";
+
+export default function NewClaimsContent() {
+  const [loading, setLoading] = useState(false);
+  const [claims, setClaims] = useState<any>(null);
+  const searchParams = useSearchParams();
+  const patientIdFromQuery = searchParams.get("patientId");
+
+  const [claimInputs, setClaimInputs] = useState({
+    isPreAuth: false,
+    patientId: patientIdFromQuery || "",
+    doctorName: "DR. ",
+    tpaName: "",
+    insuranceCompany: "",
+    status: "",
+    description: "",
+    preAuth: "",
+    OTHER: "",
+    additionalNotes: "",
+    PAST_INVESTIGATION: "",
+    CURRENT_INVESTIGATION: "",
+    CLINIC_PAPER: "",
+    ICP: "",
+  });
+  const router = useRouter();
+  const params = useParams();
+  const id = params.id;
+  const handleCreateClaim = async (value = null) => {
+    try {
+      const {
+        CLINIC_PAPER,
+        PAST_INVESTIGATION,
+        CURRENT_INVESTIGATION,
+        OTHER,
+        ICP,
+        preAuth,
+        status,
+        ...others
+      } = claimInputs;
+      const payload = {
+        ...others,
+        documents: [
+          CLINIC_PAPER,
+          ICP,
+          PAST_INVESTIGATION,
+          CURRENT_INVESTIGATION,
+          ...(OTHER || []), // if OTHER is an array, ensure it's not null
+        ].filter(Boolean),
+      };
+      setLoading(true);
+      if (value) {
+        const res = await createClaims({ ...payload, status: value });
+        if (res?.status == 201) {
+          setLoading(false);
+          router.push("/claims");
+        }
+      } else {
+        const res = await createClaims(payload);
+        if (res?.status == 201) {
+          setLoading(false);
+          router.push("/claims");
+        }
+      }
+    } catch (error) {
+      console.error("Upload error:", error);
+    } finally {
+      // setLoading(false);
+    }
+  };
+
+  const fetchClaims = async () => {
+    try {
+      const res = await getClaimsById(id);
+      if (res?.status == 200) {
+        setClaims(res?.data);
+      }
+    } catch (err) {
+      console.error("Failed to fetch claims:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchClaims();
+  }, []);
+
+  return (
+    <SidebarLayout>
+      {loading && <LoadingOverlay />}
+      <CreateClaim
+        handleCreateClaim={handleCreateClaim}
+        loading={loading}
+        setLoading={setLoading}
+        claimInputs={claimInputs}
+        setClaimInputs={setClaimInputs}
+      />
+    </SidebarLayout>
+  );
+}
